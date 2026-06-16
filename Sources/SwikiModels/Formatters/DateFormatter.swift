b01cc550672e0849753b46b48137c26ab4bc33b4
Kirill Tila <72096479+Tixster@users.noreply.github.com>
2026-06-16 12:15:26 +0300

    sendable iso formatter

--- Sources/SwikiModels/Formatters/DateFormatter.swift
+++ Sources/SwikiModels/Formatters/DateFormatter.swift
@@ -12,18 +12,32 @@ extension DateFormatter {
 
 extension ISO8601DateFormatter {
 
-    public static let fractional: ISO8601DateFormatter = {
-        let fractional = ISO8601DateFormatter()
-        fractional.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+    public static let fractional: SendableISO8601Formatter = {
+        let fractional = SendableISO8601Formatter(formatOptions: [.withInternetDateTime, .withFractionalSeconds])
         return fractional
     }()
 
-    public static let basic: ISO8601DateFormatter = {
-        let fractional = ISO8601DateFormatter()
-        fractional.formatOptions = [.withInternetDateTime]
+    public static let basic: SendableISO8601Formatter = {
+        let fractional = SendableISO8601Formatter(formatOptions: [.withInternetDateTime])
         return fractional
     }()
 
 }
 
-extension ISO8601DateFormatter: @unchecked @retroactive Sendable {}
+
+public final class SendableISO8601Formatter: @unchecked Sendable {
+
+    private let lock = NSLock()
+    private let formatter: ISO8601DateFormatter
+
+    public init(formatOptions: ISO8601DateFormatter.Options) {
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = formatOptions
+        self.formatter = formatter
+    }
+
+    public func date(from string: String) -> Date? {
+        lock.withLock { formatter.date(from: string) }
+    }
+
+}
