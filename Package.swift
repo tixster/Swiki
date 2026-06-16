ff953d73e2cb573f0239e2ea7dfed085304deb03
Kirill Tila <72096479+Tixster@users.noreply.github.com>
2026-06-16 12:20:47 +0300

    update swift version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -62,5 +62,6 @@ let package = Package(
             name: "SwikiTests",
             dependencies: ["Swiki", "SwikiModels", "SwikiGraphQLOperationGenerator"]
         ),
-    ]
+    ],
+    swiftLanguageModes: [.v6]
 )
