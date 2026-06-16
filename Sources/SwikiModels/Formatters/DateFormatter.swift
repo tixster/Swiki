090e3a3da9ef27ee445aacb1393c49ae2a43464b
Kirill Tila <72096479+Tixster@users.noreply.github.com>
2026-06-16 11:56:06 +0300

    optimized date formatter

--- Sources/SwikiModels/Formatters/DateFormatter.swift
+++ Sources/SwikiModels/Formatters/DateFormatter.swift
@@ -2,10 +2,28 @@ import Foundation
 
 extension DateFormatter {
 
-    static let yyyyMMdd: DateFormatter = {
+    public static let yyyyMMdd: DateFormatter = {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
         return dateFormatter
     }()
 
 }
+
+extension ISO8601DateFormatter {
+
+    public static let fractional: ISO8601DateFormatter = {
+        let fractional = ISO8601DateFormatter()
+        fractional.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+        return fractional
+    }()
+
+    public static let basic: ISO8601DateFormatter = {
+        let fractional = ISO8601DateFormatter()
+        fractional.formatOptions = [.withInternetDateTime]
+        return fractional
+    }()
+
+}
+
+extension ISO8601DateFormatter: @unchecked @retroactive Sendable {}
